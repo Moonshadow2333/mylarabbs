ee7c590e0ba5d5cb8c377f02debd4156f760a30b
moonshadow <1815265375@qq.com>
2022-02-27 02:31:42 +0000

    生成用户认证代码

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -17,10 +17,10 @@
       <!-- Right Navbar -->
       <ul class="navbar-nav navbar-right">
         <li class="nav-item">
-          <a class="nav-link" href="#">注册</a>
+          <a class="nav-link" href="{{route('register')}}">注册</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">登录</a>
+          <a class="nav-link" href="{{route('login')}}">登录</a>
         </li>
       </ul>
     </div>
--- resources/views/shared/_messages.blade.php
+++ resources/views/shared/_messages.blade.php
@@ -1,10 +1,9 @@
 @foreach(['success','info','warning','danger'] as $msg)
 @if(session()->has($msg))
 <div class="flash-message">
-    <div class="alert alert-{{$msg}} alert-dismissible fade show">
-    <button type="button" class="close" data-dismiss="alert">&times;</button>
-    {{session()->get($msg)}}
-  </div>
+    <div class="alert alert-{{$msg}}">
+        {{session()->get($msg)}}
+    </div>
 </div>
 @endif
 @endforeach
