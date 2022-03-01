169631da6b1c08cb4c1c957182f2196938ba2e56
moonshadow <1815265375@qq.com>
2022-03-01 01:33:17 +0000

    认证后的消息提醒

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function root(){
-        session()->flash('info','this is a test!');
         return view('pages.root');
     }
 }
