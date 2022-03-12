a35eef3fdbfa04c4b190f1a8286cac4ded1c7c47
moonshadow <1815265375@qq.com>
2022-03-12 02:32:18 +0000

    翻译队列化

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -2,15 +2,18 @@
 
 namespace App\Observers;
 use App\Models\Topic;
-use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 class TopicObserver
 {
     public function saving(Topic $topic){
         $topic->body = clean($topic->body,'user_topic_body');
 
         $topic->excerpt = make_excerpt($topic->body);
+
+    }
+    public function saved(Topic $topic){
         if(!$topic->slug){
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            dispatch(new TranslateSlug($topic));
         }
     }
 }
