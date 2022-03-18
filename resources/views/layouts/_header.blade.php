9281b62881c2c7e7d0f0196ce36c121bfc49377a
moonshadow <1815265375@qq.com>
2022-03-18 05:09:48 +0000

    消息通知列表

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -43,6 +43,11 @@
         </li>
         @else
          <!-- Dropdown -->
+       <li class="nav-item notification-badge">
+         <a href="{{route('notifications.index')}}" class="nav-link mr-3 badge badge-pill badge-{{Auth::user()->notification_count > 0 ? 'hint' : 'secondary'}} text-white">
+           {{Auth::user()->notification_count}}
+         </a>
+       </li>
        <li class="nav-item">
           <a href="{{route('topics.create')}}" class="nav-link mt-1 mr-3 font-weight-bold">
             <i class="fa fa-plus"></i>
