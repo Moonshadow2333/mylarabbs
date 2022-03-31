0509c6ac0cd3ad92b2b965001a46d2d9913af478
moonshadow <1815265375@qq.com>
2022-03-31 08:13:38 +0000

    站点设置

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,7 +4,7 @@
       由Moonshadow编码设计
     </p>
     <p class="float-end">
-      <a href="mailto:1815265375@qq.com">
+      <a href="mailto:{{setting('contact_email')}}">
         联系我们
       </a>
     </p>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{csrf_token()}}">
-  <title>@yield('title','LaraBBS') - laravel进阶课程</title>
-  <meta name="description" content="@yield('description','LaraBBS 爱好者社区')">
+  <title>@yield('title','LaraBBS') - {{setting('site_name','Laravel 进阶教程')}}</title>
+  <meta name="description" content="@yield('description',setting('seo_description','LaraBBS 爱好者社区'))">
+  <meta name="keywords" content="@yield('keyword', setting('seo_keyword', 'LaraBBS,社区,论坛,开发者论坛'))" />
   <!-- css -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="/css/mycss1.css">
