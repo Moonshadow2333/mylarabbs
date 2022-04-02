b3cd235b9152885b987d022ff495a9c4cbf6811c
moonshadow <1815265375@qq.com>
2022-04-02 01:35:49 +0000

    活跃用户

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable implements MustVerifyEmailContract
      *
      * @var array<int, string>
      */
+    use Traits\ActiveUserHelper;
     protected $fillable = [
         'name',
         'email',
